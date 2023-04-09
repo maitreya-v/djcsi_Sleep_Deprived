d41d41e2cdb707582487c10594966ce0a9af5a78
unknown <vijay.harkare2020@gmail.com>
2023-04-09 07:01:27 +0530

    add rest apis

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import Register, Login, Logout, Otp, Web_Block, Enable, Disable
+from .views import Register, Login, Logout, Otp, Web_Block, Enable, Disable, Screen, Set_threshold, SafeSearch, App_screen, Visualize
 
 urlpatterns = [
     path('api/register/', Register, name='register'),
@@ -10,5 +10,10 @@ urlpatterns = [
     path('api/otp/', Otp, name='otp'),
     path('api/web_block/', Web_Block, name='web_block'),
     path('api/enable/', Enable, name='enable'),
-    path('api/disable/', Disable, name='disable')
+    path('api/disable/', Disable, name='disable'),
+    path('screen/', Screen, name='screen'),
+    path('set_threshold', Set_threshold, name='set_threshold'),
+    path('safesearch/', SafeSearch, name='safesearch'),
+    path('appscreen/', App_screen, name='app_screen'),
+    path('visualize', Visualize, name='visualize')
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -11,15 +11,94 @@ from .otp import *
 from django_otp import verify_token
 # from ..parental_control_system.settings import *
 from django.conf import settings
-import requests
+import requests, time
+
+# main user for session
+usersession = None
 
 # Web-site blocker function
-number_requests_total = 0
+blocked = [] # custom blocked website
+# number_requests_total = 0
 
 # Visited web site UNIX timestamp with block boolean (whether blocked or not)
 
 # To enable and disable parental controls
 enable = False
+
+# Active time for each app.
+total_screen = {
+    'Monday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Tuesday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Wednesday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Thursday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Friday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Saturday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Sunday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0}
+} # Will be set when Screen API is called
+
+threshold = {
+    'Monday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Tuesday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Wednesday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Thursday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Friday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Saturday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0},
+    'Sunday': {'11pmto7am': 0, '7amto11am': 0, '11amto3pm': 0, '3pmto7pm': 0, '7pmto11pm': 0}
+}
+# For apps. Dummy app -> will contain a dictionary of each individual session duration with id
+active = {
+    'dummy': {
+        'total': 50, # Total time spent on app
+        'i': 2, # i is basically id iterator
+        '1': 20, # 20 minutes app time in first session
+        '2': 30 # 30 minutes app time in second session SImilarly other apps will have
+    }
+}
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def Visualize(request):
+    return Response(total_screen, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def App_screen(request):
+    global active
+    app_screen = request.data.get('app_screen')
+    name = request.data.get('name')
+    i += 1
+    active[name]['i'] = i
+    active[name][i] = app_screen
+    active[name]['total'] += app_screen
+    return Response({'ok': 'app screen time recorded for this session'}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def Screen(request):
+    global total_screen, threshold
+    total_time = request.data.get('screentime')
+    day = request.data.get('day')
+    slot = request.data.get('slot')
+    total_screen[day][slot] += total_time
+    if total_screen[day][slot] > threshold[day][slot]:
+        send_mail(
+                settings.OTP_EMAIL_SUBJECT,
+                f'Screen time threshold for {day} and {slot} has been crossed.',
+                settings.OTP_EMAIL_SENDER,
+                [usersession.email],
+                fail_silently=False,
+            )
+    return Response({'ok': 'true'}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def Set_threshold(request):
+    global threshold
+    day = request.data.get('day')
+    slot = request.data.get('slot')
+    threshold_user = request.data.get('threshold')
+    threshold[day][slot] = threshold_user
+    return Response({'ok':'threshold set'}, status=status.HTTP_200_OK)
 
 # Create your views here.
 @api_view(['POST'])
@@ -41,9 +120,11 @@ timeout_end = None
 def Login(request):
     global temp_otp
     global timeout_start
+    global usersession
     email = request.data.get('email')
     password = request.data.get('password')
     user = authenticate(request, email=email, password=password)
+    usersession = user
     # device = create_totp_device(user)
     if user is not None:
         token, created = Token.objects.get_or_create(user=user)
@@ -128,3 +209,44 @@ def Disable(request):
     global enable
     enable = False
     return Response({'disable': 'True'}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def SafeSearch(request):
+    API_KEY = 'AIzaSyD0SW87UPDScKW3jgpn6jFxQPUUG6e_WFo'
+    SEARCH_ENGINE_ID = 'e2df6f3212e754b14'
+    # SEARCH_QUERY = 'Microsoft'
+    SEARCH_QUERY = request.data.get('query')
+    MAX_RESULTS = 100
+
+    url = f'https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={SEARCH_QUERY}&safe=active'
+    url_template = 'https://www.googleapis.com/customsearch/v1?key={}&cx={}&q={}&safe=active&start={}'
+    response = requests.get(url)
+
+    # parse the response data
+    data = response.json()
+
+    # # loop through the search results and display the title and link for each result
+    # for item in data['items']:
+    #     # check if SafeSearch blocked this result
+    #     if 'blockReasons' in item:
+    #         print(f"Blocked result: {item['title']} ({item['link']})")
+    #         print(f"Reason: {item['blockReasons'][0]['reason']}")
+    #         print()
+    #     else:
+    #         print(f"Title: {item['title']}")
+    #         print(f"Link: {item['link']}")
+    #         print()
+    results = []
+    for i in range(1, MAX_RESULTS+1, 10):
+        url = url_template.format(API_KEY, SEARCH_ENGINE_ID, SEARCH_QUERY, i)
+        response = requests.get(url)
+        data = response.json()
+        results += data['items']
+
+    # for item in results:
+    #     print(f"Title: {item['title']}")
+    #     print(f"Link: {item['link']}")
+    #     print()
+
+    return Response({'results': results}, status=status.HTTP_200_OK)
